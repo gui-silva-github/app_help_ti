df6fdc57047dc9be802da07f9f153067cbeb73b0
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-08 14:19:11 -0300

    Navegação e Rodapé do App Help Ti

---
+++ htdocs/app_help_ti/partials/navegacao.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Ti</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        a, a:hover{
+            text-decoration: none;
+        }
+        .p, a:hover{
+            color: white;
+        }
+      .card-home {
+        padding: 30px 0 0 0;
+        width: 100%;
+        margin: 0 auto;
+      }
+      .card-abrir-chamado, .card-consultar-chamado{
+        margin-top: 1.5em;
+        margin-bottom: 0.5rem;
+        width: 100%;
+      }
+    </style>
+
+</head>
+<body>
+    <nav class='navbar navbar-dark bg-dark justify-content-around'>
+        <a class='navbar-brand' href='#'>
+            <img src='../imagens/logo.png' width='30' height='30' class='d-inline-block align-top' alt='logo'>
+            App Help Ti
+        </a>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+            <a class='nav-link' href='../logoff.php'>SAIR</a>
+            </li>
+        </ul>
+    </nav>
+
+   
---
+++ htdocs/app_help_ti/partials/rodape.php
@@ -0,0 +1,11 @@
+    <footer class='navbar navbar-dark bg-dark justify-content-around'>
+            <a class='navbar-brand text-secondary' target="_blank" href='https://github.com/gui-silva-github/'>
+                Gui Silva Github
+            </a>
+            <p class="p mt-3">
+                Todos os direitos reservados
+            </p>
+            <a class='text-secondary' target="_blank" href='https://github.com/stars/gui-silva-github/lists/php'>Todos projetos PHP</a>
+    </footer>
+</body>
+</html>
